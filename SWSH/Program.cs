7676991ed9b32f1f8b748ff44b73a4e6b14c85ba
Nabeel Omer <nabeelkomer@gmail.com>
2017-12-25 00:52:07 +0530

    [WIP] Changes to __start

--- SWSH/Program.cs
+++ SWSH/Program.cs
@@ -30,7 +30,7 @@ namespace SWSH
         static void Main(string[] args)
         {
             Console.Title = $"SWSH - {__version()}";
-             __checkHash(args.Any((x) => x == "--IgnoreChecksumMismatch"));
+            __checkHash(args.Any((x) => x == "--IgnoreChecksumMismatch"));
             Console.Write("swsh --help or -h for help.\n\n");
             __start();
         }
@@ -41,24 +41,20 @@ namespace SWSH
                 __color($"{_workingDirectory.Replace('\\', '/').Remove(0, 2).ToLower()}:", ConsoleColor.DarkCyan);
                 __color("swsh> ", ConsoleColor.DarkGray);
                 _command = __getCommand();
-                if (_command.StartsWith("swsh"))
-                {
-                    _command = _command.Replace("swsh", "").Trim();
-                    if (_command == "--version" || _command == "-v") __version();
-                    else if (_command.StartsWith("--add") || _command.StartsWith("-a")) __addConnection();
-                    else if (_command.StartsWith("--help") || _command.StartsWith("-h")) __interactiveHelp();
-                    else if (_command.StartsWith("--connect") || _command.StartsWith("-c")) __connect();
-                    else if (_command.StartsWith("--show")) __show();
-                    else if (_command.StartsWith("--delete")) __delete();
-                    else if (_command.StartsWith("--edit")) __edit();
-                    else if (_command.StartsWith("--keygen")) __keygen();
-                    else if (_command == "clear") __clear();
-                    else __help();
-                }
+                if (_command == "--version" || _command == "-v") __version();
+                else if (_command.StartsWith("--add") || _command.StartsWith("-a")) __addConnection();
+                else if (_command.StartsWith("--help") || _command.StartsWith("-h")) __interactiveHelp();
+                else if (_command.StartsWith("--connect") || _command.StartsWith("-c")) __connect();
+                else if (_command.StartsWith("--show")) __show();
+                else if (_command.StartsWith("--delete")) __delete();
+                else if (_command.StartsWith("--edit")) __edit();
+                else if (_command.StartsWith("--keygen")) __keygen();
+                else if (_command == "clear") __clear();
                 else if (_command == "ls") __ls();
                 else if (_command.StartsWith("cd")) __cd();
                 else if (_command.StartsWith("upload")) __upload();
                 else if (_command.Trim() != "") __color($"ERROR: SWSH -> {_command} -> unknown command.\n", ConsoleColor.Red);
+                else __help();
             }
             catch (Exception exp)
             {
