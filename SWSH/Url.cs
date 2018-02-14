7d437adef2628cb1d9ec99deaf06c409af1b7d58
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-02-14 15:43:38 +0530

    Added Url.cs

---
+++ SWSH/Url.cs
@@ -0,0 +1,8 @@
+﻿namespace SWSH {
+    public static class Url {
+        public const string
+            Keygen = "https://github.com/muhammadmuzzammil1998/SWSH/tree/master/swsh-keygen#swsh-keygen",
+            Checksum = "https://raw.githubusercontent.com/muhammadmuzzammil1998/SWSH/master/checksum",
+            License = "https://raw.githubusercontent.com/muhammadmuzzammil1998/SWSH/master/LICENSE";
+    }
+}
