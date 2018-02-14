df05eb6e204e1f5f90789e609f70ba28db75d381
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2018-02-15 00:12:52 +0530

    Updated urls

--- SWSH/Url.cs
+++ SWSH/Url.cs
@@ -1,8 +1,8 @@
 ﻿namespace SWSH {
     public static class Url {
         public const string
-            Keygen = "https://github.com/muhammadmuzzammil1998/SWSH/tree/master/swsh-keygen#swsh-keygen",
-            Checksum = "https://raw.githubusercontent.com/muhammadmuzzammil1998/SWSH/master/checksum",
-            License = "https://raw.githubusercontent.com/muhammadmuzzammil1998/SWSH/master/LICENSE";
+            Keygen = "https://github.com/SecureWindowsShell/swsh-keygen",
+            Checksum = "https://raw.githubusercontent.com/SecureWindowsShell/SWSH/master/checksum",
+            License = "https://raw.githubusercontent.com/SecureWindowsShell/SWSH/master/LICENSE";
     }
 }
